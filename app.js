0164d683c956d847d9ee7c624e8aca1eeb87ac0f
Alex Carroll <alexcarroll.mn@gmail.com>
2017-04-11 16:10:53 -0500

    adding handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var path = require('path');
+var express = require('express'),
+	exphbs  = require('express-handlebars'),
+	path = require('path');
 
 /*****************************
 	    App Variables
@@ -7,13 +8,24 @@ var path = require('path');
 var app = express();
 
 
+var hbs = exphbs.create({
+    defaultLayout: 'main',
+
+    partialsDir: [
+        'views/partials/'
+    ]
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use('/vendor', express.static(path.join(__dirname, 'bower_components')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.set('port', process.env.PORT || 5000);
 app.get("/", function (req, res) {
 
-	res.send("<h1>hello</h1>");
+	res.render("home", {body: "hello"});
 
 });
 
