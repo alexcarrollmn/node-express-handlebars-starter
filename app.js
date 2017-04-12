f7c4be076f8f9f5cd04868e669d8fda88696aa72
Alex Carroll <alexcarroll.mn@gmail.com>
2017-04-12 10:42:47 -0500

    adding some css

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ var express = require('express'),
 	exphbs  = require('express-handlebars'),
 	path = require('path');
 
-/*****************************
-	    App Variables
-******************************/
 var app = express();
-
 
 var hbs = exphbs.create({
     defaultLayout: 'main',
@@ -22,13 +18,14 @@ app.set('view engine', 'handlebars');
 app.use('/vendor', express.static(path.join(__dirname, 'bower_components')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.set('port', process.env.PORT || 5000);
 app.get("/", function (req, res) {
 
 	res.render("home", {body: "hello"});
 
 });
 
+
+app.set('port', process.env.PORT || 5000);
 
 app.listen(app.get('port'), function () {
   console.log('Server starting');
