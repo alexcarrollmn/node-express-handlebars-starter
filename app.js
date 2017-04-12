79a217c18f03172f923fb04fc66192c1a9ff81ab
Alex Carroll <alexcarroll.mn@gmail.com>
2017-04-12 12:54:43 -0500

    some comments on app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var express = require('express'),
-	exphbs  = require('express-handlebars'),
-	path = require('path');
+const express = require('express'),
+	  exphbs  = require('express-handlebars'),
+	  path = require('path');
 
 var app = express();
 
+//sets up templating with handlebars
 var hbs = exphbs.create({
     defaultLayout: 'main',
 
@@ -15,6 +16,7 @@ var hbs = exphbs.create({
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+//give me some static paths
 app.use('/vendor', express.static(path.join(__dirname, 'bower_components')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
@@ -24,7 +26,7 @@ app.get("/", function (req, res) {
 
 });
 
-
+//let's start this thing!
 app.set('port', process.env.PORT || 5000);
 
 app.listen(app.get('port'), function () {
